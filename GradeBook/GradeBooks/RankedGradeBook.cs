11b7c9b900b41e821d6113b37971864948a1180f
Santiago Martinez <35016987+samart333@users.noreply.github.com>
2020-04-24 09:44:30 -0500

    Upload all files to the project

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        RankedGradeBook(string name) : base(name)
+        {
+            Type = Enums.GradeBookType.Ranked;
+        }
+    }
+}
